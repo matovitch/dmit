758055bbd31d89f1f2d37810cc9c84b5d051ab8e
Camille BRUGEL <camille.brugel@laposte.net>
2021-10-31 12:41:46 +0000

    Reproduce file hierarchy in container for remote debuging

--- env/env.sh
+++ env/env.sh
@@ -5,41 +5,34 @@ PROJECT_NAME=$(basename $ROOT_FOLDER | perl -pe 's/([^_])([A-Z])/\1_\2/g' | tr '
 
 function make()
 {
-    tup && /mnt/test/data/diff.sh && /mnt/bin/test/test -tse=inout
+    tup && $HOST_ROOT/test/data/diff.sh && $HOST_ROOT/bin/test/test -tse=inout
 }
 
 function cleanSource()
 {
-    find /mnt -type f \( -name "*.cpp"   -o \
-                         -name "*.hpp"   -o \
-                         -name "*.h"     -o \
-                         -name "*.sh"    -o \
-                         -name "*.gv"    -o \
-                         -name "Tupfile" -o \
-                         -name "*.tup"      \
-                                            \) | while read file; do sed -i 's/ *$//' $file; done
+    find $HOST_ROOT -type f \( -name "*.cpp"   -o \
+                               -name "*.hpp"   -o \
+                               -name "*.h"     -o \
+                               -name "*.sh"    -o \
+                               -name "*.gv"    -o \
+                               -name "Tupfile" -o \
+                               -name "*.tup"      \
+                                                  \) | while read file; do sed -i 's/ *$//' $file; done
 }
 
 function dmitServer()
 {
-    /mnt/bin/src/dmit_server.bin --url tcp://127.0.0.1:4567 > dmit_server.log&
+    $HOST_ROOT/bin/src/dmit_server.bin --url tcp://127.0.0.1:4567 > dmit_server.log&
 }
 
 function dmitClient()
 {
-    /mnt/bin/src/dmit_client.bin --url tcp://127.0.0.1:4567 $@
+    $HOST_ROOT/bin/src/dmit_client.bin --url tcp://127.0.0.1:4567 $@
 }
 
 function makeEnv()
 {
     docker build -t ${PROJECT_NAME}:env -f ${ROOT_FOLDER}/env/env.Dockerfile ${ROOT_FOLDER}/env
-}
-
-function makeCompileDb()
-{
-    tup compiledb
-    sed "s~/mnt~${HOST_ROOT}~" -i /mnt/bin/compile_commands.json
-    mv /mnt/bin/compile_commands.json /mnt
 }
 
 function runEnv()
@@ -53,16 +46,16 @@ function runEnv()
     docker inspect $CONTAINER 2> /dev/null | grep running > /dev/null &&
         docker container rm -f $CONTAINER 2>&1 > /dev/null
 
-    docker run --privileged                                            \
-               --cap-add ALL                                           \
-               --user guest:guest                                      \
-               --device /dev/fuse                                      \
-               --mount type=bind,source=${ROOT_FOLDER},target=/mnt     \
-               --workdir /mnt                                          \
-               --name $CONTAINER                                       \
-               --interactive                                           \
-               --tty                                                   \
-               --env HOST_ROOT=$ROOT_FOLDER                            \
-               -p 5678:5678                                            \
+    docker run --privileged                                                \
+               --cap-add ALL                                               \
+               --user guest:guest                                          \
+               --device /dev/fuse                                          \
+               --mount type=bind,source=${ROOT_FOLDER},target=$ROOT_FOLDER \
+               --workdir $ROOT_FOLDER                                      \
+               --name $CONTAINER                                           \
+               --interactive                                               \
+               --tty                                                       \
+               --env HOST_ROOT=$ROOT_FOLDER                                \
+               -p 5678:5678                                                \
                $IMAGE $@
 }
